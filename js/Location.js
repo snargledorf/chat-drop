825343f3e05b1be6d28a52a866bbb141ff19188a
Ryan Esteves <snargledorf@gmail.com>
2017-07-27 11:41:14 -0400

    [RE] Initial commit

---
+++ js/Location.js
@@ -0,0 +1,59 @@
+(function() {
+
+  LocationMonitor = function() {
+    var listeners = [];
+    this.currentLocation = null;
+
+    var locationWatchId = navigator.geolocation.watchPosition(function(loc) {
+        this.currentLocation = loc.coords;
+        notifyOfLocationChange();
+    }, function(error) {
+        console.log(error);
+    });
+
+    this.locationChanged = function(listener) {
+      listeners.push(listener);
+      
+      // Check if we have a recent cached location
+      if (currentLocation) {
+        listener(currentLocation);
+      }
+    };
+
+    this.removeListener = function(listener) {
+      var indexOfListener = listeners.indexOf(listener);
+      if (indexOfListener == -1)
+        return;
+
+      listeners.splice(indexOfListener, 1);
+    };
+
+    this.close = function() {      
+      listeners.length = 0;
+      navigator.geolocation.clearWatch(locationWatchId);
+    };
+
+    function notifyOfLocationChange() {
+      for (var i = 0; i < listeners.length; i++) {
+        var listener = listeners[i];
+        listener(this.currentLocation);
+      }
+    };
+  };
+
+  Location = {
+    getCurrentLocation: function(callback) {
+      if (!Location.locationSupported())
+        return;
+
+      navigator.geolocation.getCurrentPosition(function(loc) {
+        callback(loc.coords);
+      }, function(error) {
+          console.log(error);
+      });
+    },
+    locationSupported: function() {
+      return "geolocation" in navigator;
+    }
+  };
+})();
---
+++ js/Message.js
@@ -0,0 +1,136 @@
+(function() {
+    
+    var database = firebase.database();
+    var chatMessagesRef = database.ref("chat-messages");
+    var messageLocationsRef = database.ref("message-locations");
+    var messageLocationsGeoFire = new GeoFire(messageLocationsRef);
+    
+    var MessageImpl = function(id, text, uid) {
+        this.id = id;
+        this.text = text;
+        this.uid = uid;
+    };
+
+    MessageImpl.prototype.setLocation = function(loc, callback) {
+        messageLocationsGeoFire.set(this.id, [loc.latitude, loc.longitude]).then(function() {
+            if (callback) {
+                callback();
+            }
+        });
+    };
+
+    var NearbyMessage = function(id, locationQuery) {
+        this.id = id;
+
+        var nbm = this;
+
+        this.leftMessageArea = function(listener) {
+            locationQuery.on("key_exited", function(key) {
+                if (key !== id) {
+                    return;
+                }
+
+                listener(nbm);
+            });
+        };
+    };
+
+    NearbyMessageMonitor = function(locationMonitor, radius) {
+        var messageNearbyListeners = [];
+        var locationQuery = null;
+
+        // Begin monitoring for location changes and updating
+        // our query location as we move
+        var locationChangedListener = function(loc) {
+            var criteria = {
+                center: [loc.latitude, loc.longitude],
+                radius: radius
+            };
+
+            if (locationQuery) {
+                locationQuery.updateCriteria(criteria);
+            } else {
+                // Create a location query based on our current location
+                locationQuery = messageLocationsGeoFire.query(criteria);
+
+                // Listen for when we enter an area with a message
+                locationQuery.on("key_entered", function(key, location, distance) {
+                    // Notify the listener of a nearby message
+                    var nearbyMessage = new NearbyMessage(key, locationQuery);
+                    notifyNearbyMessageListeners(nearbyMessage);
+                });
+            }
+        };
+
+        locationMonitor.locationChanged(locationChangedListener);
+
+        this.messageNearby = function(listener) {
+            messageNearbyListeners.push(listener);
+        };
+
+        this.removeListener = function(listener) {
+            var index = messageNearbyListeners.indexOf(listener);
+            if (index > -1) {
+                messageNearbyListeners.splice(index, 1);
+            }
+        }
+
+        this.stopMonitoring = function() {
+            messageNearbyListeners.length=0;
+            
+            if (!locationChangedListener) {
+                return;
+            }
+
+            locationMonitor.removeListener(locationChangedListener);
+        };
+
+        function notifyNearbyMessageListeners(nearbyMessage) {
+            for (var i = 0; i < messageNearbyListeners.length; i++) {
+                messageNearbyListeners[i](nearbyMessage);
+            }
+        }
+    };
+    
+    Message = {
+        create: function(text, uid, location, callback) {
+            var messageObj = {uid:uid, text:text};
+            chatMessagesRef.push(messageObj).then(function(snapshot) {
+                var id = snapshot.key;
+                var message = new MessageImpl(snapshot.key, text, uid);
+                message.setLocation(location, function() {                    
+                    if (callback) {
+                        callback(message);
+                    }
+                });
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+        },
+        delete: function(id, callback) {
+            messageLocationsGeoFire.remove(id).then(function(){
+                chatMessagesRef.child(id).remove().then(function() {
+                    if (callback) {
+                        callback();
+                    }
+                });
+            });
+        },
+        get: function(id, callback) {
+            chatMessagesRef.child(id).once("value", function(snapshot) {
+                if (!snapshot.exists()) {
+                    return;
+                }
+                
+                var msgObj = snapshot.val();
+
+                var message = new MessageImpl(id, msgObj.text, msgObj.uid);
+
+                if (callback) {
+                    callback(message);
+                }
+            });
+        }
+    };
+})();
